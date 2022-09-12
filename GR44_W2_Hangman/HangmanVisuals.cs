66ca42effb12f08c10e24193ee4a170a104c7605
Janne <janne@inyan.se>
2022-09-12 14:22:36 +0200

    Add project files.

---
+++ GR44_W2_Hangman/HangmanVisuals.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Text;
+
+namespace GR44_W2_Hangman
+{
+    internal class HangmanVisuals
+    {
+        /*
+            0   __
+            1  |  |
+            2  |  O
+            3  | /|\
+            4  | / \
+            5  |
+            6----------
+             0123456789
+        */
+
+        char[,] Visuals;
+        int[][,] VisualParts;
+
+        public HangmanVisuals()
+        {
+            Visuals = new char[7, 10]; // 7 rows, 10 columns
+
+            for (int row = 0; row <= Visuals.GetUpperBound(0); row++)
+                for (int col = 0; col <= Visuals.GetUpperBound(1); col++)
+                    Visuals[row, col] = ' ';
+
+            InitRow(0, "   __");
+            InitRow(1, "  |  |");
+            InitRow(2, "  |  O");
+            InitRow(3, "  | /|\\");
+            InitRow(4, "  | / \\");
+            InitRow(5, "  |");
+            InitRow(6, "----------");
+
+            VisualParts = new int[10][,];
+
+            VisualParts[0] = new int[,] { { 6, 0 }, { 6, 1 }, { 6, 2 }, { 6, 3 }, { 6, 4 },
+                                          { 6, 5 }, { 6, 6 }, { 6, 7 }, { 6, 8 }, { 6, 9 } };
+            VisualParts[1] = new int[,] { { 5, 2 }, { 4, 2 }, { 3, 2 }, { 2, 2 }, { 1, 2 } };
+            VisualParts[2] = new int[,] { { 0, 3 }, { 0, 4 } };
+            VisualParts[3] = new int[,] { { 1, 5 } };
+            VisualParts[4] = new int[,] { { 2, 5 } };
+            VisualParts[5] = new int[,] { { 3, 5 } };
+            VisualParts[6] = new int[,] { { 3, 4 } };
+            VisualParts[7] = new int[,] { { 3, 6 } };
+            VisualParts[8] = new int[,] { { 4, 4 } };
+            VisualParts[9] = new int[,] { { 4, 6 } };
+        }
+
+        public int PartsCount { get { return VisualParts.Length; } }
+
+        public (int row, int col, char chr)[] GetPart(int part)
+        {
+            List<(int row, int col, char chr)> Ut = new();
+
+            if (part > VisualParts.GetUpperBound(0))
+                throw new ArgumentException("Too high part-arg");
+
+            int[,] theParts = VisualParts[part];
+
+            for (int i = 0; i <= theParts.GetUpperBound(0); i++)
+            {
+                var row = theParts[i, 0];
+                var col = theParts[i, 1];
+
+                Ut.Add((row, col, Visuals[row,col]));
+            }
+
+            return Ut.ToArray();
+        }
+
+        private void InitRow(int row, string rowString)
+        {
+            if (row -1 > Visuals.GetUpperBound(0))
+                throw new ArgumentException("Too high row number");
+
+            if (rowString.Length -1 > Visuals.GetUpperBound(1))
+                throw new ArgumentException("Too many chars in row");
+
+            for (int index = 0; index < rowString.Length; index++)
+                if (rowString[index] != ' ')
+                    Visuals[row, index] = rowString[index];
+        }
+
+    }
+}
---
+++ GR44_W2_Hangman/Program.cs
@@ -0,0 +1,70 @@
+﻿
+HangmanVisuals Visuals = new();
+
+int VisualsBaseRow = 1;
+int WordBaseRow = VisualsBaseRow + 7;
+int BaseCol = 10;
+var file = AppDomain.CurrentDomain.BaseDirectory + "FruitsAndBerries.txt";
+string[] Words = File.ReadAllLines(file);
+int randomWorkIndex = new Random().Next(Words.Length);
+string TheWord = Words[randomWorkIndex].ToUpper();
+List<char> Guesses = new();
+
+WriteLine("HANGMAN - Fruits and Berries");
+WriteAtPosition(WordBaseRow, 0, "Word:");
+WriteAtPosition(WordBaseRow, BaseCol, GetHiddenWord(TheWord, Guesses));
+WriteAtPosition(WordBaseRow + 1, 0, "Guesses:  ");
+
+int Counter = 0;
+string HiddenWord;
+
+do
+{
+    char Guess;
+    do { Guess = ReadKey(true).KeyChar.ToString().ToUpper()[0];} 
+    while (Guesses.Contains(Guess));
+
+    if (!TheWord.Contains(Guess))
+        WriteVisualPart(Counter++);
+
+    Guesses.Add(Guess);
+    HiddenWord = GetHiddenWord(TheWord, Guesses);
+    WriteAtPosition(WordBaseRow, BaseCol, HiddenWord);
+    WriteAtPosition(WordBaseRow + 1, BaseCol + Guesses.Count -1, Guess.ToString());
+} while (Counter < Visuals.PartsCount && HiddenWord.Contains("_"));
+
+if (Counter < Visuals.PartsCount)
+    WriteAtPosition(WordBaseRow + 3, 0, "Great, you got it!\n");
+else
+    WriteAtPosition(WordBaseRow + 3, 0, $"Sorry, you lost! (the word was {TheWord})\n");
+
+
+
+
+
+void WriteAtPosition(int row, int col, string str)
+{
+    SetCursorPosition(col, row);
+    Write(str);
+}
+
+void WriteVisualPart(int part)
+{
+    foreach (var item in Visuals.GetPart(part))
+    {
+        SetCursorPosition(item.col + BaseCol, item.row + VisualsBaseRow);
+        Write(item.chr);
+    }
+}
+
+string GetHiddenWord(string word, List<char> guesses)
+{
+    StringBuilder sb = new();
+    foreach (char c in word)
+        if (guesses.Contains(c))
+            sb.Append(c);
+        else
+            sb.Append("_");
+
+    return sb.ToString();
+}
